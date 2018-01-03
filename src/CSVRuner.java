5418bde8817adbb753a912b7e8dd9b39bbc701c2
William Andress <bill.andress@gmail.com>
2018-01-03 13:20:35 -0500

    salmon

--- src/CSVRuner.java
+++ src/CSVRuner.java
@@ -1,8 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class CSVRuner {
 	public static void main(String[] args) {
 		String name = "";		
 		String csvName = TabToComma.reWrite(name);
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(csvName));
 			
+		} catch(Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 }
