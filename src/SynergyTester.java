34b44f5ab3509c580ca51efa0aaefcdaf138fe5e
Steven QU <steven7214@Stevens-MacBook-Air.local>
2018-01-05 14:44:36 -0500

    me second

--- src/SynergyTester.java
+++ src/SynergyTester.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public class SynergyTester {
 
-	public static ArrayList<Double> compareGenes(String combinationName, double[] vital,
+	public static double[] compareGenes(String combinationName, double[] vital,
 			HashMap<String, double[]> genes) {
-		ArrayList<Double> analysis = new ArrayList<Double>();
+		double[] analysis = new double[2];
 		String[] geneNames = combinationName.split("\t"); // gets individual genes from combinationName
 		for (String geneName : geneNames) {
 			genes.get(geneName)[0] -= vital[0]; // subtracts vital status of those in the combination
@@ -18,19 +18,18 @@ public class SynergyTester {
 		double percentage2 = genes.get(geneNames[1])[0] / (genes.get(geneNames[1])[0] + genes.get(geneNames[1])[1]);
 		double combinationPercentage = vital[0] / (vital[0] + vital[1]); // finds combined percentage
 		if (combinationPercentage > percentage1 && combinationPercentage > percentage2) { // comparison analysis
-			analysis.add(combinationPercentage - percentage1);
-			analysis.add(combinationPercentage - percentage2);
-		} else if (combinationPercentage > percentage1 || combinationPercentage > percentage2)
-			analysis.add(combinationPercentage - (percentage1 + percentage2) / 2);
-
-		return analysis;
+			analysis[0] = combinationPercentage - percentage1;
+			analysis[1] = combinationPercentage - percentage2;
+			return analysis;
+		} 
+		return null;
 	}
 
 	public static void main(String[] args) {
 		HashMap<String, double[]> combinations = new HashMap<String, double[]>();
 		HashMap<String, double[]> genes = new HashMap<String, double[]>();
 		File folder = new File("Data/Glioma Raw/Mutation Annotation Raw");
-		// File folder = new File("Data/Test");
+		// File folder = new File("Data/Glioma Raw/BigBoys");
 		File[] listOfFiles = folder.listFiles();
 		ArrayList<Person> people = new ArrayList<Person>();
 		ArrayList<String> patientNames = new ArrayList<String>();
@@ -41,6 +40,7 @@ public class SynergyTester {
 			patientNames.add(listOfFiles[count].getName().substring(0, 12).toLowerCase()); // adds all names from
 																							// mutations files
 		}
+
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("Data/Glioma Raw/Clinical.txt"));
 			BufferedReader reader2;
@@ -73,9 +73,6 @@ public class SynergyTester {
 
 		for (Person person : people) {
 			ArrayList<String[]> personCombos = person.getCombinations(); // adds all combinations of two genes and their
-			System.out.println(personCombos.size());
-			if (personCombos.size() == 1830)
-				break;
 			ArrayList<String[]> personGenes = person.getMutations(); // add all vital rates of individual genes
 			for (String[] personGene : personGenes) {
 				if (genes.containsKey(personGene[0])) {
@@ -113,35 +110,22 @@ public class SynergyTester {
 				}
 
 			}
-			System.out.println(genes.size());
-			System.out.println(combinations.size());
-			try {
-
-				BufferedWriter writer = new BufferedWriter(new FileWriter("output/SynergyTest.txt"));
-				int count = 0;
-				for (String currentKey : combinations.keySet()) {
-					ArrayList<Double> output = SynergyTester.compareGenes(currentKey, combinations.get(currentKey),
-							genes);
-					if (output.size() == 0)
-						continue;
-					String lineToWrite = currentKey + "\t" + output.get(0);
-					if (output.size() == 2)
-						lineToWrite += "\t" + output.get(1);
-					writer.write(lineToWrite + "\n");
-
-					if (count > 100000) {
-						writer.flush();
-						count = 0;
-					} else {
-						count++;
-					}
-
-				}
-				writer.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			System.out.println("Done");
 		}
+		
+		try {
+
+			BufferedWriter writer = new BufferedWriter(new FileWriter("output/SynergyTest.csv"));
+			for (String currentKey : combinations.keySet()) {
+				double[] output = SynergyTester.compareGenes(currentKey, combinations.get(currentKey), genes);
+				if (output == null)
+					continue;
+				String lineToWrite = currentKey + "," + output[0] + "," + output[1];
+				writer.write(lineToWrite + "\n");
+			}
+			writer.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Done");
 	}
 }
