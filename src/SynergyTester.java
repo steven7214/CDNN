1b7eb043d94a9172bf1e08d5246f58a0d44b8daf
Steven QU <steven7214@Stevens-MacBook-Air.local>
2018-01-05 09:32:42 -0500

    1/4/18

--- src/SynergyTester.java
+++ src/SynergyTester.java
@@ -5,15 +5,15 @@ public class SynergyTester {
 	
 	public static ArrayList<Double> compareGenes(String combinationName, double[] vital, HashMap<String, double[]> genes) {
 		ArrayList<Double> analysis = new ArrayList<Double>();
-		String[] geneNames = combinationName.split("\t");
+		String[] geneNames = combinationName.split("\t"); //gets individual genes from combinationName
 		for (String geneName: geneNames) {
-			genes.get(geneName)[0] -= vital[0];
+			genes.get(geneName)[0] -= vital[0]; //subtracts vital status of those in the combination
 			genes.get(geneName)[1] -= vital[1];
 		}
-		double percentage1 = genes.get(geneNames[0])[0]/(genes.get(geneNames[0])[0] + genes.get(geneNames[0])[1]);
-		double percentage2 = genes.get(geneNames[1])[0]/(genes.get(geneNames[0])[0] + genes.get(geneNames[1])[1]);
-		double combinationPercentage = vital[0]/(vital[0]+vital[1]);
-		if (combinationPercentage > percentage1 && combinationPercentage > percentage2) {
+		double percentage1 = genes.get(geneNames[0])[0]/(genes.get(geneNames[0])[0] + genes.get(geneNames[0])[1]); //finds individual percentages
+		double percentage2 = genes.get(geneNames[1])[0]/(genes.get(geneNames[1])[0] + genes.get(geneNames[1])[1]);
+		double combinationPercentage = vital[0]/(vital[0]+vital[1]); //finds combined percentage
+		if (combinationPercentage > percentage1 && combinationPercentage > percentage2) { //comparison analysis
 			analysis.add(combinationPercentage-percentage1);
 			analysis.add(combinationPercentage-percentage2);
 		}
@@ -26,11 +26,14 @@ public class SynergyTester {
 		HashMap<String, double[]> combinations = new HashMap<String, double[]>();
 		HashMap<String, double[]> genes = new HashMap<String, double[]>();
 		File folder = new File("Data/Glioma Raw/Mutation Annotation Raw");
+		//File folder = new File("Data/Test");
 		File[] listOfFiles = folder.listFiles();
 		ArrayList<Person> people = new ArrayList<Person>();
 		ArrayList<String> patientNames = new ArrayList<String>();
 		String line;
 		for (int count = 0; count < listOfFiles.length; count++) {
+			if (listOfFiles[count].getName().equalsIgnoreCase(".DS_Store"))
+				continue;
 			patientNames.add(listOfFiles[count].getName().substring(0, 12).toLowerCase()); // adds all names from
 																							// mutations files
 		}
@@ -66,7 +69,7 @@ public class SynergyTester {
 		
 		for (Person person : people) {
 			ArrayList<String[]> personCombos = person.getCombinations(); // adds all combinations of two genes and their
-																			// vital rates
+			System.out.println(personCombos.size());
 			ArrayList<String[]> personGenes = person.getMutations(); // add all vital rates of individual genes
 			for (String[] personGene : personGenes) {
 				if (genes.containsKey(personGene[0])) {
@@ -85,6 +88,7 @@ public class SynergyTester {
 				}
 
 			}
+			
 			for (String[] personCombo : personCombos) {
 
 				String key = personCombo[0] + "\t" + personCombo[2];
@@ -104,30 +108,30 @@ public class SynergyTester {
 					combinations.put(personCombo[0] + "\t" + personCombo[2], value);
 				}
 			}
-			
 		}
+		System.out.println(genes.size());
+		System.out.println(combinations.size());
 		try {
-			PrintWriter writer = new PrintWriter(new FileWriter("output/SynergyTest.txt"));
+			BufferedWriter writer = new BufferedWriter(new FileWriter("output/SynergyTest.txt"));
 			int count = 0;
 			for (String currentKey : combinations.keySet()) {
 				ArrayList<Double> output = SynergyTester.compareGenes(currentKey, combinations.get(currentKey), genes);
-				
-				
-				String lineToWrite = "";
-				if(output.size() == 1) {
-					lineToWrite =  currentKey + "\t" + output.get(0);
-					writer.println(lineToWrite);
-				} else if(output.size() == 2) {
-					lineToWrite =  currentKey + "\t" + output.get(0) + "\t" + output.get(1);
-					writer.println(lineToWrite);
-				}
-				
+				if (output.size() == 0)
+					continue;
+				String lineToWrite =  currentKey + "\t" + output.get(0);
+				 if(output.size() == 1) 
+					lineToWrite += "\t" + output.get(1);
+				writer.write(lineToWrite + "\n");
+				writer.flush();
+			/*	
 				if(count > 100000) {
 					writer.flush();
 					count = 0;
 				} else {
 					count++;
-				}
+				}*/
+				
+				
 			}
 			writer.close();
 			System.out.println("done");
