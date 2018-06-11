657087517e235ecdb4c0b3653705e6fa59b96773
Steven Qu <stevenqu2001@hotmail.com>
2018-06-11 09:51:21 -0400

    oops

--- Python/CancerSeek.py
+++ Python/CancerSeek.py
@@ -12,41 +12,35 @@ import os
 
 numpy.random.seed(7)
 
-filename = os.path.join( os.getcwd(), '..', 'Data/CancerSEEK/Only Numbers (normal).csv' )
-data = numpy.loadtxt(filename, delimiter=",")
+filename = os.path.join( os.getcwd(), '..', 'Data/CancerSEEK/Training Data.csv' )
+traindata = numpy.loadtxt(filename, delimiter=",")
+filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Validation Data.csv')
+testdata = numpy.loadtxt(filename, delimiter=",")
+
 #split data
-input = data[:, 0:39]
-output = data[:, 39]
+train = [traindata[:, 0:39], traindata[:, 39]]
+test = [testdata[:, 0:39], testdata[:, 39]]
 
-#define 10-fold cross validation
-kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state = 7)
+# Parameters: node number, epochs, regulizer
+parameters = [5, 20, 0.0005]
+update = [5, 10, 0.0005]
 
-# Parameters: node number, regularizer, epochs
-parameters = [30, 0.03, 50]
-update = [5, 0.01, 10]
-
-optimizeIndex = 1
 temp = 0
 while optimizeIndex <= 2:
-    list = []
-    for train, test in kfold.split(input, output):
-        #build model
-        model = Sequential()
-        #model.add(Dropout(0.2, input_shape=(39,)))
-        model.add(Dense(parameters[0], input_dim=39,  kernel_regularizer=regularizers.l2(parameters[1]), activation='relu'))
-        model.add(Dense(1, activation='sigmoid'))
-        #compile model
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        #fit model
-        model.fit(input[train], output[train], epochs=parameters[2], batch_size=32, verbose=0)
-        #evaluate model
-        scores = model.evaluate(input[test], output[test], verbose=0)
-        print(scores)
-        accuracy = scores[1]*100
-        print("\n%s: %.2f%%" % (model.metrics_names[1], accuracy))
-        list.append(accuracy)
+    #build model
+    model = Sequential()
+    #model.add(Dropout(0.2, input_shape=(39,)))
+    model.add(Dense(parameters[0], input_dim=39, kernel_regularizer=regularizers.l2(parameters[2]), activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    #compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #fit model
+    model.fit(train[0], train[1], epochs=parameters[1], batch_size=32, verbose = 0)
+    #evaluate model
+    scores = model.evaluate(test[0], test[1], verbose = 0)
+    accuracy = scores[1]*100
+    print("\n%s: %.2f%%" % (model.metrics_names[1], accuracy))
     #print("weights: " + str(model.layers[0].get_weights()))
-    accuracy = numpy.mean(list)
     print("average = " + str(accuracy))
     if temp == 0:
         temp = accuracy
@@ -54,15 +48,15 @@ while optimizeIndex <= 2:
     elif accuracy-temp > 0.1:
         temp = accuracy
         parameters[optimizeIndex] += update[optimizeIndex]
+        optimizeIndex += 1
     else:
         parameters[optimizeIndex] -= update[optimizeIndex]
         optimizeIndex -= 1
-        temp = 0
+        temp = accuracy
     if optimizeIndex > 2:
         break
     print(str(optimizeIndex) + " " + str(parameters[optimizeIndex]))
 
-print("\n" + "%.2f%% (+/- %.2f%%)" % (numpy.mean(list), numpy.std(list)))
 #calculate prediction
 predictions = model.predict(input)
 #round predictions
