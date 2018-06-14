7e6f125954ac8cab7f874a130895d531f1b94d4b
Steven Qu <stevenqu2001@hotmail.com>
2018-06-14 13:04:05 -0400

    ten fold cross validation against false positive

--- Python/CancerSeekSplit.py
+++ Python/CancerSeekSplit.py
@@ -3,45 +3,65 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import regularizers
+from sklearn.model_selection import StratifiedKFold
 import numpy
 import os
-from DataSplit import getData
 
 numpy.random.seed(7)
 
-average = 0
-num = 0
-for i in range(5):
-    #randomly split data into train, validation, test
-    filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Only Numbers (normal).csv')
-    trainData, validationData, testData = getData(filename, 0.1, 0.2, False)
+#get complete data set
+filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Only Numbers (normal).csv')
+totalData = numpy.loadtxt(filename, delimiter=",")
 
-    '''filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Training Data.csv' )
-    trainData = numpy.loadtxt(filename, delimiter=",")
-    filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Validation Data.csv')
-    validationData = numpy.loadtxt(filename, delimiter=",")
-    filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Test Data.csv')
-    testData = numpy.loadtxt(filename, delimiter=",")'''
+#randomly split data into train, validation, test
+#trainData, validationData, testData = getData(filename, 0.1, 0.2, False)
 
+'''filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Training Data.csv' )
+trainData = numpy.loadtxt(filename, delimiter=",")
+filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Test Data.csv')
+testData = numpy.loadtxt(filename, delimiter=",")'''
 
-    #split data
-    train = [trainData[:, 0:39], trainData[:, 39]]
-    #validation = [validationData[:, 0:39], validationData[:, 39]]
-    test = [testData[:, 0:39], testData[:, 39]]
+#split data
+total = [totalData[:, 0:39], totalData[:, 39], totalData[:, 40]]
+#define 10-fold cross validation
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
 
+num = 1
+for train, test in kfold.split(total[0], total[1]):
     model = Sequential()
     model.add(Dense(35, input_dim=39, kernel_regularizer=regularizers.l2(0.0002), activation='relu'))
     model.add(Dense(20, kernel_regularizer=regularizers.l2(0), activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.fit(train[0], train[1], epochs=150, batch_size=32, verbose = 0)
+    #class_weight makes false positives less desirable
+    model.fit(total[0][train], total[1][train], class_weight={0: 1, 1: 0.5}, epochs=150, batch_size=32, verbose = 0)
 
-    accuracy = model.evaluate(train[0], train[1], verbose = 0)
-    print("train: " + str(accuracy[1]*100))
-    accuracy = model.evaluate(test[0], test[1], verbose = 0)
-    print("test: " + str(accuracy[1]*100) + "\n")
-    average += accuracy[1]*100
+    '''accuracy = model.evaluate(train[0], train[1], verbose = 0)
+    print("train: " + str(accuracy[1]*100))'''
+    accuracy = model.evaluate(total[0][test], total[1][test], verbose = 0)
+    print("total: " + str(accuracy[1]*100) + "\n")
+
+    #calculate prediction
+    predictions = model.predict(total[0][test])
+    predictions = predictions.tolist()
+    #round predictions
+    rounded = []
+    for prediction in predictions:
+        rounded.append(round(prediction[0]))
+
+    #add cancer types
+    types = total[2][test]
+
+    #add real cancer value
+    real = total[1][test]
+
+    #create file to write in
+    filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/CrossValidation/results ' + str(num) + '.csv')
+    file = open(filename, 'w')
+    #change to add cancer type
+    for count in range(len(rounded)):
+        line = str(real[count]) + "," + str(rounded[count]) + "," + str(types[count])
+        file.write(line + "\n")
+    file.close()
     num += 1
-average /= num
-print("average= " + str(average))
