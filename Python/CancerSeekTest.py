143b739815708d90af0ea647f5b0e02bf7cef447
Steven Qu <stevenqu2001@hotmail.com>
2018-06-14 10:26:08 -0400

    hi

--- Python/CancerSeekTest.py
+++ Python/CancerSeekTest.py
@@ -9,9 +9,12 @@ from DataSplit import getData
 
 numpy.random.seed(7)
 
+#get complete data set
+filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Only Numbers (normal).csv')
+totalData = numpy.loadtxt(filename, delimiter=",")
+
 #randomly split data into train, validation, test
-'''filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Only Numbers (normal).csv')
-trainData, validationData, testData = getData(filename, 0.1, 0.2, False)'''
+#trainData, validationData, testData = getData(filename, 0.1, 0.2, False)
 
 filename = os.path.join(os.getcwd(), '..', 'Data/CancerSEEK/Training Data.csv' )
 trainData = numpy.loadtxt(filename, delimiter=",")
@@ -21,6 +24,7 @@ testData = numpy.loadtxt(filename, delimiter=",")
 #split data
 train = [trainData[:, 0:39], trainData[:, 39]]
 test = [testData[:, 0:39], testData[:, 39]]
+total = [totalData[:, 0:39], totalData[:, 39]]
 
 model = Sequential()
 model.add(Dense(35, input_dim=39, kernel_regularizer=regularizers.l2(0.0002), activation='relu'))
@@ -28,18 +32,19 @@ model.add(Dense(20, kernel_regularizer=regularizers.l2(0), activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#class_weight makes false positives less desirable
 model.fit(train[0], train[1], class_weight={0: 1, 1: 0.5}, epochs=150, batch_size=32, verbose = 0)
 
 accuracy = model.evaluate(train[0], train[1], verbose = 0)
 print("train: " + str(accuracy[1]*100))
-accuracy = model.evaluate(test[0], test[1], verbose = 0)
-print("test: " + str(accuracy[1]*100) + "\n")
+accuracy = model.evaluate(total[0], total[1], verbose = 0)
+print("total: " + str(accuracy[1]*100) + "\n")
 
 #calculate prediction
-predictions = model.predict(test[0])
+predictions = model.predict(total[0])
 #round predictions
-rounded = [round(test[0]) for test in predictions]
-output = test[0].tolist()
+rounded = [round(total[0]) for total in predictions]
+output = total[0].tolist()
 for count in range(len(rounded)):
     output[count].append(rounded[count])
 #create file to write in
